203052a6b37a8e2d1357e0b7b2f2a955b3c53fea
Renukamehta13 <renukamehta1316@gmail.com>
2024-06-26 00:45:54 +0530

    Update Terraform_Code.tf

--- Terraform_Code.tf
+++ Terraform_Code.tf
@@ -1,8 +1,11 @@
 provider "aws" 
 {
-region = "Mumbai"
+region = "ap-south-1"
 }
 
 resource "aws_instance" "foo"
 {
-ami = 
+ami = " ami-0f58b397bc5c1f2e8 "
+instance_type = " t2.micro "
+tag = { Name = " TF = Instance " }
+}
